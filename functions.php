eb8af4a0ca4457859f22e1b5cf1f91944a3368ba
Paul Barthmaier <paul@barthmaier.rocks>
2021-11-13 12:36:17 -0500

    editor style

--- functions.php
+++ functions.php
@@ -6,13 +6,13 @@
 
 if ( ! function_exists( 'pbrocks_fse_support' ) ) :
 	function pbrocks_fse_support() {
+		add_theme_support( 'editor-styles' );
+		add_theme_support( 'wp-block-styles' );
 		add_theme_support( 'automatic-feed-links' );
 		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'wp-block-styles' );
 		add_theme_support( 'responsive-embeds' );
-		add_theme_support( 'editor-styles' );
-		add_editor_style( 'style.css' );
 		add_theme_support( 'custom-units' );
+		add_editor_style( 'style-editor.css' );
 	}
 	add_action( 'after_setup_theme', 'pbrocks_fse_support' );
 endif;
@@ -22,26 +22,35 @@ add_action( 'wp_enqueue_scripts', 'pbrocks_fse_scripts' );
  * Enqueue scripts and styles.
  */
 function pbrocks_fse_scripts() {
-	wp_enqueue_style( 'pbrocks-fse', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style(
+		'pbrocks-fse',
+		get_template_directory_uri() . '/style.css',
+		array(),
+		// wp_get_theme()->get( 'Version' )
+		time()
+	);
 	wp_enqueue_style( 'dashicons' );
 	wp_enqueue_style(
 		'pbrocks-fse-fonts-style',
 		pbrocks_fse_fonts_url(),
 		array(),
-		wp_get_theme()->get( 'Version' )
+		// wp_get_theme()->get( 'Version' )
+		time()
 	);
 	wp_enqueue_script(
 		'pbrocks-fse-navigation',
 		get_template_directory_uri() . '/assets/js/navigation.js',
 		array( 'jquery' ),
-		wp_get_theme()->get( 'Version' ),
+		// wp_get_theme()->get( 'Version' )
+		time(),
 		false
 	);
 	wp_enqueue_script(
 		'pbrocks-fse-alignment',
 		get_template_directory_uri() . '/assets/js/alignment.js',
 		array( 'jquery' ),
-		wp_get_theme()->get( 'Version' ),
+		// wp_get_theme()->get( 'Version' )
+		time(),
 		false
 	);
 }
@@ -53,16 +62,18 @@ add_action( 'enqueue_block_editor_assets', 'pbrocks_fse_editor_scripts' );
  */
 function pbrocks_fse_editor_scripts() {
 	wp_enqueue_style(
-		'pbrocks-fse-style',
-		get_template_directory_uri() . '/style.css',
+		'pbrocks-fse-editor-style',
+		get_template_directory_uri() . '/assets/css/style-editor.css',
 		array(),
-		wp_get_theme()->get( 'Version' )
+		// wp_get_theme()->get( 'Version' )
+		time()
 	);
 	wp_enqueue_style(
 		'pbrocks-fse-fonts-style',
 		pbrocks_fse_fonts_url(),
 		array(),
-		wp_get_theme()->get( 'Version' )
+		// wp_get_theme()->get( 'Version' )
+		time()
 	);
 }
 
@@ -94,6 +105,9 @@ function pbrocks_fse_fonts_url() {
 add_filter( 'get_the_archive_title_prefix', 'pbrocks_fse_remove_archive_title_prefix' );
 /**
  * Remove archive title prefix.
+ *
+ * @param strting $prefix
+ * @return void
  */
 function pbrocks_fse_remove_archive_title_prefix( $prefix ) {
 	$prefix = '';
@@ -111,14 +125,16 @@ function pbrocks_fse_remove_archive_title_prefix( $prefix ) {
  * Block styles.
  */
 if ( function_exists( 'register_block_style' ) ) {
-require get_template_directory() . '/inc/block-styles.php';
+	require get_template_directory() . '/inc/block-styles.php';
 }
 
-add_action( 'admin_menu', 'pbrocks_fse_add_custom_css_menu_item' );
+add_action( 'admin_menu', 'pbrocks_fse_add_back_customizer' );
 /**
  * Add an Appearance sub-menu link to Additional CSS.
+ *
+ * @return void
  */
-function pbrocks_fse_add_custom_css_menu_item() {
+function pbrocks_fse_add_back_customizer() {
 	add_submenu_page(
 		'themes.php',
 		esc_html__( 'Customizer', 'pbrocks-fse' ),
